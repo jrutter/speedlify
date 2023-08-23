c7da1ce64d8831aae06375bdc1f3cffd92b30de1
Jake Rutter <jakerutter@Rutter-Central.local>
2023-08-22 21:03:50 -0400

    updated sites

--- _data/sites/vfc.js
+++ _data/sites/vfc.js
@@ -2,13 +2,12 @@ module.exports = {
 	name: "Sample", // optional, falls back to object key
 	description: "The default sites that get tested",
 	options: {
-		runs: 3,
-		frequency: 60, // (in minutes)
+		runs: 1,
+		frequency: 60 * 23, // (in minutes)
+        freshChrome: "run"
 	},
 	urls: [
-		"https://www.thenorthface.com/",
         "https://www.vans.com/",
         "https://www.timberland.com/",
-        "https://www.dickies.com/"
 	]
 };
